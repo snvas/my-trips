e120e0c4b1d2fc69d475719b80348a842d2767a6
Soraia Novaes <sonovaes511@gmail.com>
2021-09-21 22:43:50 -0300

    places

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.my-trips.soraianovaes.com.br/',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@edtecnova',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
